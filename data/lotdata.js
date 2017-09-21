b3497ec47e3d19da086490576eb095aa12d26fc8
pekko1215 <pekko1215@yahoo.co.jp>
2017-09-21 16:48:08 +0900

    dwa

---
+++ data/lotdata.js
@@ -0,0 +1,36 @@
+/**
+ * Created by pekko1215 on 2017/07/24.
+ */
+
+var lotdata = {
+    normal: [
+        {   name:"リプレイ",
+            value:4488/32768},
+        {   name:"ベル",
+            value:3766/32768},
+        {   name:"スイカ",
+            value:210/32768},
+        {   name:"チェリー",
+            value:949/32768},
+        {   name:"強チェリー",
+            value:75/32768},
+        {   name:"BIG",
+            value:136/32768},
+        {   name:"REG",
+            value:136/32768},
+        {   name:"リールロック",
+            value:24/32768}
+    ],
+    "big":[
+        {
+            name:"JACIN",
+            value:1/7.7
+        }
+    ],
+    "jac":[
+        {
+            name:"JACGAME",
+            value:1
+        }
+    ]
+}
---
+++ data/yakudata.js
@@ -0,0 +1,72 @@
+/**
+ * Created by pekko1215 on 2017/07/15.
+ */
+var YakuData = [
+    {
+        name: "はずれ",
+        pay: [0, 0, 0]
+    },{
+        name: "リプレイ",
+        pay: [0, 0, 7]
+    },{
+        name:"ベル",
+        pay:[9,15]
+    },{
+        name:"ベル",
+        pay:[9,15]
+    },{
+        name:"スイカ",
+        pay:[1,7,7]
+    },{
+        name:"チェリー",
+        pay:[2,0,0]
+    },{
+        name:"ダミー",
+        pay:[0,0,0]
+    },{
+        name:"ダミー",
+        pay:[0,0,0]
+    },{
+        name:"BIG1",
+        pay:[0,0,0]
+    },{
+        name:"BIG2",
+        pay:[0,0,0]
+    },{
+        name:"REG",
+        pay:[0,0,0]
+    },{
+        name:"REG",
+        pay:[0,0,0]
+    },{
+        name:"リールロック",
+        pay:[3,0,0]
+    },{
+        name:"リプレイ",
+        pay:[0,0,0]
+    },{
+        name:"BIG2",
+        pay:[0,0,0]
+    },{
+        name:"7枚",
+        pay:[0,7,0]
+    },{
+        name:"7枚",
+        pay:[0,7,0]
+    },{
+        name:"BIG15",
+        pay:[0,15,0]
+    },{
+        name:"BIG15",
+        pay:[0,15,0]
+    },{
+        name:"REG15",
+        pay:[0,0,15]
+    }
+];
+(new Array(6)).fill(0).forEach(function(){
+    YakuData.push({
+        name:"アライさん1枚",
+        pay:[1,0,0]
+    })
+})
